d8e49140e65bbd83533139360da3cafbd53b7de9
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2023-11-02 10:32:37 +0100

    Remove call to `cat`, use grep instead

--- textree.sh
+++ textree.sh
@@ -2,7 +2,7 @@
 
 file=${1}
 
-cat ${file} | grep -e "part{" -e "chapter{" -e "section{" |\
+grep -e "part{" -e "chapter{" -e "section{" ${file }|\
     sed -e 's/\\part{/ /g'\
         -e 's/\\chapter{/├── /g'\
         -e 's/\\section{/│   ├── /g'\
